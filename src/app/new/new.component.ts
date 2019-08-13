12fd1761a04f5e97b2ebd376beb2bd51f1a8e76a
JT Kaufman <jtkaufman737@gmail.com>
2019-08-13 17:13:49 -0400

    stop point

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.scss']
 })
+
 export class NewComponent implements OnInit {
+  fileToUpload: File = null;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  runFileUpload(files: FileList) {
+    console.log(FileList);
+    // this.fileToUpload = files.item(0);
+    // console.log(new FileReader().readAsText(this.fileToUpload));
+  }
 }
